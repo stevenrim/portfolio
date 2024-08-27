ade50e56091c32f1c6d75bf9c1f3cc28901c7196
Steven | 9purp0s3 <stevenrim@proton.me>
2024-08-26 22:09:35 -0400

    Update about.ts

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -8,10 +8,12 @@ const createAbout = () : string[] => {
   const EMAIL = "Email";
   const GITHUB = "Github";
   const LINKEDIN = "Linkedin";
+  const MEDIUM = "Medium";
   
   const email = `<i class='fa-solid fa-envelope'></i> ${EMAIL}`;   
   const github = `<i class='fa-brands fa-github'></i> ${GITHUB}`;
   const linkedin = `<i class='fa-brands fa-linkedin'></i> ${LINKEDIN}`;
+  const medium = `<i class='fa-regular fa-file'></i> ${MEDIUM}`;
   let string = "";
 
   about.push("<br>");
@@ -35,6 +37,13 @@ const createAbout = () : string[] => {
   string += linkedin;
   string += SPACE.repeat(17 - LINKEDIN.length);  
   string += `<a target='_blank' href='https://www.linkedin.com/in/${command.social.linkedin}'>linkedin/${command.social.linkedin}</a>`;
+  about.push(string);
+
+  string = '';
+  string += SPACE.repeat(2);
+  string += medium;
+  string += SPACE.repeat(17 - MEDIUM.length);  
+  string += `<a target='_blank' href='https://medium.com/@${command.social.medium}'>medium/${command.social.medium}</a>`;
   about.push(string);
 
   about.push("<br>");
